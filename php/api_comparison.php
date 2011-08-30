b010963b8ff892feb3bb8a579c627265607b10f5
fox <fox91@anche.no>
2011-08-30 15:10:04 +0200

    namespace fix + minor

--- php/api_comparison.php
+++ php/api_comparison.php
@@ -88,14 +88,34 @@ function get_links($lang, $page, $wgRequest,
     $conds = array ('pl_from = '.$dbr->strencode ($pageClass->get_id ()));
 
     $links = $dbr->select (array('pagelinks'),
-                            array('pl_title'),
+                            array('pl_title', 'pl_namespace'),
                             $conds, array('LIMIT' => 50000));
 
     if (!count($links))
         toDie ("There are no outer links");
     $data = array();
     foreach ($links as $id => $link) {
-        $data[] = str_replace(" ", "_", $link["pl_title"]);
+        if (!is_numeric($link["pl_title"][0])) {
+            // skip pages that start with a number (e.g.: years)
+            if ($link['pl_namespace'] == 0) {
+                $data[] = str_replace(" ", "_", $link["pl_title"]);
+            }
+            else if ($link['pl_namespace'] == 1) {
+                $data[] = "Talk:".str_replace(" ", "_", $link["pl_title"]);
+            }
+            else if ($link['pl_namespace'] == 2) {
+                $data[] = "User:".str_replace(" ", "_", $link["pl_title"]);
+            }
+            else if ($link['pl_namespace'] == 3) {
+                $data[] = "User_Talk:".str_replace(" ", "_", $link["pl_title"]);
+            }
+            else if ($link['pl_namespace'] == 4) {
+                $data[] = "Wikipedia:".str_replace(" ", "_", $link["pl_title"]);
+            }
+            else if ($link['pl_namespace'] == 12) {
+                $data[] = "Help:".str_replace(" ", "_", $link["pl_title"]);
+            }
+        }
     }
     return array_unique($data);
 }
@@ -112,7 +132,7 @@ $article2 = trim(str_replace(array('&#39;', '%20'), array('\'', ' '),
                             $wgRequest->getSafeVal('a2')));
 $article2 = urldecode($article2);
 if (!($article1 && $article2 && $lang1 && $lang2)) {
-    toDie("Please specify an article name");
+    toDie("Please specify two articles and their language");
 }
 
 $links1 = get_links($lang1, $article1, $wgRequest,
@@ -123,6 +143,7 @@ $links2 = get_links($lang2, $article2, $wgRequest,
 //links1 must be the min list
 //echo count($links1)."\n";
 //echo count($links2)."\n";
+$swapped = false;
 if (count($links1) > count($links2)) {
     $tmp = $links1;
     $links1 = $links2;
@@ -131,6 +152,7 @@ if (count($links1) > count($links2)) {
     $tmp = $lang1;
     $lang1 = $lang2;
     $lang2 = $tmp;
+    $swapped = true;
 }
 
 //Comparing the links lists!
@@ -157,7 +179,7 @@ for ($i=0; $i<(count($links1)/10.0); $i++) {
             if (array_key_exists("langlinks", $elem)) {
                 foreach ($elem["langlinks"] as $ll) {
                     if ($ll["lang"] == $lang2) {
-                        $result[] = str_replace(" ", "_", $ll["*"]);
+                        $result[$elem["title"]] = str_replace(" ", "_", $ll["*"]);
                         break;
                     }
                 }
@@ -173,22 +195,59 @@ for ($i=0; $i<(count($links1)/10.0); $i++) {
     }
 }
 
-foreach ($result as $link) {
+$output = array("matching" => array(),
+                "nonmatching1" => array(),
+                "nonmatching2" => array());
+$matching = array();
+foreach ($result as $original => $langlink) {
     //echo $link;
-    if (in_array($link, $links2)) {
+    if (in_array($langlink, $links2)) {
+        if (!$swapped) {
+            $output["matching"][] = array($original, $langlink);
+        }
+        else {
+            $output["matching"][] = array($langlink, $original);
+        }
+        $matching[] = $langlink;
         $match += 1;
         //echo " matches <br/>";
     }
     else {
+        if (!$swapped) {
+            $output["nonmatching1"][] = $original;
+        }
+        else {
+            $output["nonmatching2"][] = $original;
+        }
         $no_match += 1;
         //echo " doesn't match <br/>";
+    }
+}
+
+foreach ($links2 as $link) {
+    if (!(in_array($link, $matching))) {
+        if (!$swapped) {
+            $output["nonmatching2"][] = $link;
+        }
+        else {
+            $output["nonmatching1"][] = $link;
+        }
     }
 }
 
 //echo $match."\n";
 //echo $no_match;
 $exectime = microtime(true) - $time_start;
-print_result(array("result" => $match/count($links1),
-                   "exectime" => $exectime));
+if (count($links1) > 3) {
+    $res = $match/count($links1);
+}
+else {
+    $res = "N/A";
+}
+
+$output["result"] = $res;
+$output["exectime"] = $exectime;
+
+print_result($output);
 
 ?>
